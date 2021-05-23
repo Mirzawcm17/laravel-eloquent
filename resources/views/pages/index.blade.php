2c98d808a4d4e07423dbb0a6c19f5b2dfb7aea7e
Mirza <mirza.hajdarevic@medieinstitutet.se>
2021-05-23 17:06:50 +0200

    en bra start nu rullar vi

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.Locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name', 'LSAAP')}}</title>
+    </head>
+    <body>
+    <h1>Welcome to Oddworld</h1>
+    <p>This is the laravel meme application</p>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PagesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     // return view('welcome');
+//     return <h1>Hello! Follow me?. OKEYH</h1>
+// });
+
+Route::get('/', [PagesController::class,'index']);
+
+Route::get('/about', function() {
+    return view('pages.about');
 });
 
-Auth::routes();
+// Route::get('/users/{id}/{name}', function($id, $name) {
+//     return 'This is user '.$name.' with an id of '.$id;
+// });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
