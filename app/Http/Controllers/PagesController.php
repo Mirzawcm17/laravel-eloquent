a39e22c8c255106a41433f430c20178a284b4557
Mirza <mirza.hajdarevic@medieinstitutet.se>
2021-05-24 03:28:13 +0200

    compilat css och installerat bootstrap ui

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,14 +8,20 @@ class PagesController extends Controller
 {
     public function index(){
         $title = 'Welcome to Oddworld memes!';
-        return view('pages.index', compact('title'));
+        // return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title);
     }
 
     public function about(){
-        return view('pages.about');
+        $title = 'About us';
+        return view('pages.about')->with('title', $title);
     }
 
     public function services(){
-        return view('pages.services');
+        $data =array(
+            'title' => 'Services',
+            'services' => ['Web Design', 'Programming', 'Meming']
+        );
+        return view('pages.services')->with($data);
     }
 }
