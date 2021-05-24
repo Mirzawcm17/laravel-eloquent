5212dfecf506d6d2ae114e1c0178c7dbe8d8cdde
Mirza <mirza.hajdarevic@medieinstitutet.se>
2021-05-24 16:42:30 +0200

    nu funkar bättre layout rolf

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,7 +5,11 @@
     {!! Form::open(['action' => 'App\Http\Controllers\PostsController@store', 'method' => 'POST']) !!}
         <div class="form-group">
             {{Form::label('title', 'Title')}}
-            {{Form::text('title', '', ['class' => 'form-contorl', 'placeholder' => 'Title'])}}
+            {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
+        </div>
+        <div class="form-group">
+            {{Form::label('body', 'Body')}}
+            {{Form::textarea('body', '', ['class' => 'form-control', 'placeholder' => 'Body Text'])}}
         </div>
     {!! Form::close()!!}
 @endsection
