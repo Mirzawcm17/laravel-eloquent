47b2cbaa046fd575a1b968fb3ca9dffa13f3e5f9
Mirza <mirza.hajdarevic@medieinstitutet.se>
2021-05-24 20:21:56 +0200

    nu kan man edita och rateda posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,8 +4,15 @@
     <a href="/posts" class="btn btn-outline-secondary">Go Back</a>
     <h1>{{$post->title}}</h1>
     <div>
-        {{$post->body}}
+        {!!$post->body!!}
     </div>
     <hr>
     <small>Written on {{$post->created_at}}</small>
+    <hr>
+    <a href="/posts/{{$post->id}}/edit" button type="button" class="btn btn-secondary">Edit</a>
+
+    {!!Form::open(['action' => ['App\Http\Controllers\PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+    {!!Form::close()!!}
 @endsection
